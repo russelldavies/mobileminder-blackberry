875425b0af19fb706bbd2b7383eee8a3cdfbb513
Eoin Zy <eoinzy@mmtechco.com>
2011-06-01 18:31:45 +0000

    Start work on LocalData factory + ContactsReader.java added

---
+++ src/com/spotlight/track/ContactsReader.java
@@ -0,0 +1,48 @@
+package com.spotlight.track;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import javax.microedition.pim.ContactList;
+import javax.microedition.pim.PIM;
+import javax.microedition.pim.PIMException;
+
+import net.rim.blackberry.api.pdap.BlackBerryContact;
+
+public class ContactsReader
+{
+	public static String TAG = "CVK::";		// TAG used for console output to identify specific outputs
+	public ContactsReader() throws PIMException
+	{
+		// Create list of contact
+		ContactList blackBerryContactList = (ContactList)PIM.getInstance().openPIMList(PIM.CONTACT_LIST, PIM.READ_ONLY);
+		// Retrieve contact list items, which will need to be converted into an array/vector
+		Enumeration allContacts = blackBerryContactList.items();
+		// Vector to store contacts, once converted from enum
+	    Vector blackBerryContacts = enumToVector(allContacts);
+	    // Retrive specific contact (to test)
+		//blackBerryContact = (BlackBerryContact)blackBerryContacts.elementAt(listField.getSelectedIndex());
+	    BlackBerryContact bbContact = (BlackBerryContact)blackBerryContacts.elementAt(1);
+	    // and output
+	    //displayContact(bbContact);
+        //ContactDetailsScreen contactDetailsScreen = new ContactDetailsScreen(bbContact);
+		
+	    //Get an array of all populated contact fields.
+	    int fieldsWithData[] = bbContact.getFields();
+	    System.err.println(TAG+fieldsWithData.length);
+	}
+	
+	  //Convert the list of contacts from an Enumeration to a Vector
+	  private Vector enumToVector(Enumeration contactEnum) {
+	    Vector v = new Vector();
+
+	    if (contactEnum == null)
+	      return v;
+
+	    while (contactEnum.hasMoreElements()) {
+	      v.addElement(contactEnum.nextElement());
+	    }
+
+	    return v;
+	  }
+}
