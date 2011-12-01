bbf8c75ac0c3a865dfa84fc1184acff3174e9295
Russell Davies <russell@zeroflux.net>
2011-12-01 21:59:55 +0000

    Minor formatting updates on LocationMonitor

--- src/com/mmtechco/mobileminder/monitor/LocationMonitor.java
+++ src/com/mmtechco/mobileminder/monitor/LocationMonitor.java
@@ -1,10 +1,5 @@
 package com.mmtechco.mobileminder.monitor;
 
-/**
- * Class handles getting the GPS coordinates, and the frequency it updates
- * http://docs.blackberry.com/ja-jp/developers/deliverables/29185/GPS_overview_1679738_11.jsp
- */
-
 import javax.microedition.location.Criteria;
 import javax.microedition.location.Location;
 import javax.microedition.location.LocationException;
@@ -15,6 +10,7 @@ import com.mmtechco.mobileminder.Registration;
 import com.mmtechco.mobileminder.prototypes.LocalDataWriter;
 import com.mmtechco.mobileminder.prototypes.MMTools;
 import com.mmtechco.mobileminder.prototypes.Message;
+import com.mmtechco.mobileminder.util.ErrorMessage;
 import com.mmtechco.mobileminder.util.Logger;
 import com.mmtechco.mobileminder.util.Tools;
 import com.mmtechco.mobileminder.util.ToolsBB;
@@ -22,154 +18,110 @@ import com.mmtechco.mobileminder.util.ToolsBB;
 /**
  * Monitors and registers location based events.
  */
-public class LocationMonitor extends Thread implements LocationListener {
-	private static final String TAG = ToolsBB.getSimpleClassName(LocationMonitor.class);
-	
+//public class LocationMonitor extends Thread implements LocationListener {
+public class LocationMonitor implements LocationListener {
+	private static final String TAG = ToolsBB
+			.getSimpleClassName(LocationMonitor.class);
+
 	private LocalDataWriter actLog;
-	private int 			timer;
-	private	Logger     		logger	= Logger.getInstance();
-	public  LocationMessage 		locationMessage;
-	public  Location 		myLocation = null;
-	public	float			speed;
-	public	double 			longitude;
-	public  double			latitude;
+	private Logger logger = Logger.getInstance();
+	public LocationMessage locationMessage;
+	private int interval;
+	public Location myLocation = null;
 
+	private float speed;
+	private double longitude;
+	private double latitude;
 
-/**
- * The GPSListener constructor initialise the action store location and the interval value.
- * Calls the start() method on the thread which invokes the run() method of the thread.
- * 
- * @param inputAccess log of actions
- * @param inputGPSTimer interval value
- */
+	public LocationMonitor(LocalDataWriter actLog, int interval) {
+		this.actLog = actLog;
+		this.interval = interval;
+		locationMessage = new LocationMessage();
 
-public LocationMonitor(LocalDataWriter inputAccess, int inputGPSTimer)
-{
-	logger.log(TAG, "MyGPSListener::Constructor");
-	actLog 		= inputAccess;
-	timer 		= inputGPSTimer;
-	locationMessage	= new LocationMessage();
-	
-	// Initialise coords to 0
-	speed = 0;
-	latitude = 0;
-	longitude = 0;
-	
-	this.start();
-}
-	
-/**
- * When this function is started it poles the GPS service based on the interval specified in the constructor.
- * When the location changes this is recorded in local data access.  
-* 		Stores GPS location information about the device in the form of an action to the action log
-* <p>   Its sets the criteria for the type of location information needed.
-* 		It retrieves the location and saves it to the action log.
-*
-*/	
+		// Initialize coordinates
+		speed = 0;
+		latitude = 0;
+		longitude = 0;
+	}
 
-	public void locationUpdated(LocationProvider arg0, Location arg1)
-	{
+	public void locationUpdated(LocationProvider provider, Location location) {
+		// Polls GPS service based on interval specified in constructor. When
+		// location changes record in activity log.
 		logger.log(TAG, "GPS Location changed");
-		
+
 		Criteria criteria = new Criteria();
-		criteria.setHorizontalAccuracy(       Criteria.NO_REQUIREMENT);
-        criteria.setVerticalAccuracy(         Criteria.NO_REQUIREMENT);
-        criteria.setPreferredPowerConsumption(Criteria.NO_REQUIREMENT);
-        criteria.setCostAllowed(true);
-        
-		try 
-		{
-			myLocation = LocationProvider.getInstance(criteria).getLocation(timer);
-		} 
-		//LocationException - if the location couldn't be retrieved or if the timeout period expired 
-		//InterruptedException - if the operation is interrupted by calling reset() from another thread 
-		//SecurityException - if the calling application does not have a permission to query the location information 
-		//IllegalArgumentException - if the timeout = 0 or timeout < -1
-		catch (LocationException e)
-		{
-			// TODO Auto-generated catch block
+		criteria.setHorizontalAccuracy(Criteria.NO_REQUIREMENT);
+		criteria.setVerticalAccuracy(Criteria.NO_REQUIREMENT);
+		criteria.setPreferredPowerConsumption(Criteria.NO_REQUIREMENT);
+		criteria.setCostAllowed(true);
+
+		try {
+			myLocation = LocationProvider.getInstance(criteria).getLocation( interval);
+		} catch (LocationException e) {
+			logger.log(TAG, "Location could not be retrieved");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		catch (InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// Check if coordinates have changed...
-		if ( longitude != myLocation.getQualifiedCoordinates().getLongitude() ||
-			  latitude != myLocation.getQualifiedCoordinates().getLatitude()   )
-		{
-			speed     = myLocation.getSpeed();
+
+		// Check if coordinates have changed
+		if (longitude != myLocation.getQualifiedCoordinates().getLongitude()
+				|| latitude != myLocation.getQualifiedCoordinates()
+						.getLatitude()) {
+			speed = myLocation.getSpeed();
 			longitude = myLocation.getQualifiedCoordinates().getLongitude();
-	        latitude  = myLocation.getQualifiedCoordinates().getLatitude();
+			latitude = myLocation.getQualifiedCoordinates().getLatitude();
 		}
-        locationMessage.clearData();
-        locationMessage.setMessage(latitude, longitude, speed);
-        actLog.addMessage(locationMessage);		
+		locationMessage.clearData();
+		locationMessage.setMessage(latitude, longitude, speed);
+		actLog.addMessage(locationMessage);
 	}
-	
-	public void providerStateChanged(LocationProvider provider, int newState)
-	{
-		logger.log(TAG, "GPS Provider changed");		
+
+	public void providerStateChanged(LocationProvider provider, int newState) {
+		logger.log(TAG, "GPS Provider changed");
 	}
-	
+
 	/*
-	public void run()
-	{
-		try
-	    {
-			while(true)
-			{
-				sleep(timer);
-				
+	public void run() {
+		while (true) {
+			try {
+				sleep(interval);
+
 				Criteria criteria = new Criteria();
-				criteria.setHorizontalAccuracy(       Criteria.NO_REQUIREMENT);
-		        criteria.setVerticalAccuracy(         Criteria.NO_REQUIREMENT);
-		        criteria.setPreferredPowerConsumption(Criteria.NO_REQUIREMENT);
-		        criteria.setCostAllowed(true);
-		        
-				Location mylocation = LocationProvider.getInstance(criteria).getLocation(3);
-				//If there has been a change in the location of the device it is stored to the action log as an action of type GPS.
-				if(mylocation.getSpeed()>0)
-				{
-		            float  speed     = mylocation.getSpeed();
-					double longitude = mylocation.getQualifiedCoordinates().getLongitude();
-		            double latitude  = mylocation.getQualifiedCoordinates().getLatitude();
-		            //float  altitude  = mylocation.getQualifiedCoordinates().getAltitude();
-					//float  heading   = mylocation.getCourse();
-					
-		            gpsMessage.setMessage(latitude, longitude, speed);
-		            
-		            actLog.addMessage(gpsMessage);
-				} // end if
-			} // end while
-		} // end try
-        
-        //LocationException - if the location couldn't be retrieved or if the timeout period expired 
-        //InterruptedException - if the operation is interrupted by calling reset() from another thread 
-        //SecurityException - if the calling application does not have a permission to query the location information 
-        //IllegalArgumentException - if the timeout = 0 or timeout < -1
-        
-        catch (LocationException e)
-        {
-        	logWriter.log("myGPSListener::run::LocationException::"+e.getMessage());
-        	//actLog.addAction(true,action.TYPE_GPS,e.toString());
-        }
-        catch (InterruptedException e)
-		{
-        	logWriter.log("myGPSListener::run::InterruptedException::"+e.getMessage());
-        	//actLog.addAction(true,action.TYPE_GPS,e.toString());
-		}
-        catch (Exception e)
-		{
-        	logWriter.log("myGPSListener::run::Exception::"+e.getMessage());
-        	//actLog.addAction(true,action.TYPE_GPS,e.toString());
-		}
+				criteria.setHorizontalAccuracy(Criteria.NO_REQUIREMENT);
+				criteria.setVerticalAccuracy(Criteria.NO_REQUIREMENT);
+				criteria.setPreferredPowerConsumption(Criteria.NO_REQUIREMENT);
+				criteria.setCostAllowed(true);
 
-	}	// end run()*/
+				Location location = LocationProvider.getInstance(criteria)
+						.getLocation(3);
+				// If there has been a change in the location of the device it
+				// is
+				// stored to the action log as an action of type GPS.
+				if (location.getSpeed() > 0) {
+					float speed = location.getSpeed();
+					double longitude = location.getQualifiedCoordinates()
+							.getLongitude();
+					double latitude = location.getQualifiedCoordinates()
+							.getLatitude();
+					// float altitude =
+					// mylocation.getQualifiedCoordinates().getAltitude();
+					// float heading = mylocation.getCourse();
+
+					locationMessage.setMessage(latitude, longitude, speed);
+					actLog.addMessage(locationMessage);
+				}
+			} catch (LocationException e) {
+				actLog.addMessage(new ErrorMessage(e));
+				logger.log(TAG, e.getMessage());
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	*/
 }
-
 
 /**
  * This class implements the message interface to hold GPS event messages.
@@ -257,8 +209,8 @@ class LocationMessage implements Message {
 	 * @return a single string containing the entire message.
 	 */
 	public String getREST() {
-		return Registration.getRegID() + Tools.ServerQueryStringSeparator + type
-				+ Tools.ServerQueryStringSeparator + deviceTime
+		return Registration.getRegID() + Tools.ServerQueryStringSeparator
+				+ type + Tools.ServerQueryStringSeparator + deviceTime
 				+ Tools.ServerQueryStringSeparator + getLocation()
 				+ Tools.ServerQueryStringSeparator + getSpeed();
 	}
