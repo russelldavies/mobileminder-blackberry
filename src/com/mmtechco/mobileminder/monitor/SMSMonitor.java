ac1cce604eb169333c4e3d4a02de697efadde1e6
Russell Davies <russell@zeroflux.net>
2012-06-18 22:33:45 +0100

    Minor cleanup on SMSMonitor

--- src/com/mmtechco/mobileminder/monitor/SMSMonitor.java
+++ src/com/mmtechco/mobileminder/monitor/SMSMonitor.java
@@ -17,104 +17,67 @@ import com.mmtechco.mobileminder.prototypes.MMTools;
 import com.mmtechco.util.Logger;
 import com.mmtechco.util.ToolsBB;
 
-/**
- * 
- * MyTextListener monitors and registers text message based events.
- * 
- */
-public class SMSMonitor implements OutboundMessageListener, MessageListener {
+public class SMSMonitor implements MessageListener, OutboundMessageListener {
 	private static Logger logger = Logger.getLogger(SMSMonitor.class);
-	
-	private static MessageConnection receiver;
-	private MMTools tools = ToolsBB.getInstance();
+	private static MMTools tools = ToolsBB.getInstance();
 
-	/**
-	 * The constructor initialises the action store location and registers a
-	 * MessageListener for the device.
-	 * 
-	 * @param inputAccess
-	 *            log of actions
-	 */
+	private static MessageConnection receiver;
+
 	public SMSMonitor() {
-		logger.debug("Started");
-		
 		try {
-			receiver = (MessageConnection)Connector.open("sms://:0");
+			receiver = (MessageConnection) Connector.open("sms://:0");
 			if (receiver != null) {
 				receiver.setMessageListener(this);
 			}
 		} catch (IOException e) {
-			ActivityLog.addMessage(new ErrorMessage(e));
+			ActivityLog.addMessage(new ErrorMessage("Could not listen for SMS",
+					e));
 		}
-	}
-
-	/**
-	 * Adds a message and its address to the action log
-	 * 
-	 * @param inputStatus
-	 *            message structure
-	 * @param inputDestinationAddress
-	 *            message address
-	 * @param date
-	 * @param txtBody
-	 */
-	private void addToLog(String inputStatus, String contactNumber,
-			String date, String messageBody) {
-		logger.debug("Adding message to log");
-
-		boolean isOutgoing = inputStatus.equalsIgnoreCase("Outgoing Message") ? true
-				: false;
-		boolean delivered = false;
-
-		logger.debug("inputStatus is: " + inputStatus);
-		logger.debug("SMS Address: " + contactNumber);
-		logger.debug("Direction is: "
-				+ (isOutgoing ? "Outgoing" : "Incoming"));
-		logger.debug("Date: " + date);
-		logger.debug("SMS Body: " + messageBody);
-
-		// set contact name seperately
-		String contactName = new PhoneCallLogID(contactNumber).getName();
-		contactName= (null == contactName) ? "" : contactName;
-		
-
-		logger.debug("Adding message to log...");
-		ActivityLog.addMessage(new SMSMessage(contactNumber, contactName,
-				isOutgoing, delivered, date, messageBody));
-		logger.debug("Message added to log...");
-	}
-
-	public void notifyOutgoingMessage(Message message) {
-		logger.debug("SMS message outgoing");
-
-		String messageBody = "";
-		// If "message" is an object of type "TextMessage"
-		if (message instanceof TextMessage) {
-			messageBody = ((TextMessage) message).getPayloadText();
-		}
-		String recipient = message.getAddress();
-		// Strip off 'sms://'
-		recipient = recipient.substring(recipient.lastIndexOf('/')) + 1;
-		addToLog("Outgoing Message", recipient, tools.getDate(), messageBody);
+		logger.info("Started listening");
 	}
 
 	// In the simulator, ensure source and destination SMS ports are the same in
 	// the Network menu, this way you will receive the text you just sent, and
 	// can test notifyIncomingMessage
 	public void notifyIncomingMessage(MessageConnection conn) {
-		logger.debug("SMS message incoming");
+		logger.debug("Incoming message");
 
-		TextMessage message = null;
 		try {
-			message = (TextMessage) conn.receive();
-		} catch (Exception e) {
+			TextMessage message = (TextMessage) conn.receive();
+			String sender = message.getAddress();
+			// Strip off 'sms://'
+			sender = sender.substring(sender.lastIndexOf('/')) + 1;
+			addToLog(false, sender,
+					tools.getDate(message.getTimestamp().getTime()),
+					message.getPayloadText());
+		} catch (IOException e) {
 			ActivityLog.addMessage(new ErrorMessage(e));
 		}
-		String sender = message.getAddress();
-		// Strip off 'sms://'
-		sender = sender.substring(sender.lastIndexOf('/')) + 1;
-		addToLog("Incoming Message", sender, tools.getDate(),
-				message.getPayloadText());
+	}
+
+	public void notifyOutgoingMessage(Message message) {
+		logger.debug("SMS message outgoing");
+
+		if (message instanceof TextMessage) {
+			String recipient = message.getAddress();
+			// Strip off 'sms://'
+			recipient = recipient.substring(recipient.lastIndexOf('/')) + 1;
+			String messageBody = ((TextMessage) message).getPayloadText();
+			addToLog(true, recipient,
+					tools.getDate(message.getTimestamp().getTime()),
+					messageBody);
+		}
+	}
+
+	private void addToLog(boolean outgoing, String contactNumber, String date, String messageBody) {
+		logger.debug("Adding message to log");
+
+		// Get contact name if exists
+		String contactName = new PhoneCallLogID(contactNumber).getName();
+		contactName = (null == contactName) ? "Unknown" : contactName;
+		
+		boolean delivered = true; // No way to check delivery status
+		ActivityLog.addMessage(new SMSMessage(date, contactNumber, contactName, outgoing, delivered, messageBody));
 	}
 }
 
@@ -125,19 +88,17 @@ class SMSMessage extends com.mmtechco.mobileminder.net.Message {
 	 * <li>Device time
 	 * <li>Number of SMS recipient or sender
 	 * <li>Contact Name
-	 * <li>Start status: 0 = incoming, 1 = outgoing
-	 * <li>End status: 0 = bounced, 1 = delivered
+	 * <li>Direction: 0 = incoming, 1 = outgoing
+	 * <li>Delivery: 0 = bounced, 1 = delivered
 	 * <li>SMS content
 	 * </ul>
 	 */
-	public SMSMessage(String number, String name, boolean outgoing,
-			boolean delivered, String time, String content) {
-		super(com.mmtechco.mobileminder.net.Message.SMS, new String[] {
-				number,
-				name,
-				(outgoing ? "1" : "0"),
-				(delivered ? "1" : "0"),
-				content
-		});
+	// public SMSMessage(String number, String name, boolean outgoing, boolean
+	// delivered, String time, String content) {
+	public SMSMessage(String time, String number, String name,
+			boolean outgoing, boolean delivered, String content) {
+		super(com.mmtechco.mobileminder.net.Message.SMS,
+				new String[] { number, name, (outgoing ? "1" : "0"),
+						(delivered ? "1" : "0"), content });
 	}
 }
